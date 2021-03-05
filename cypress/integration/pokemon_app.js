a87ef1e1dac04659c78a8cd141d0cf1110bed914
Pauli Partanen <pauli.tapio.partanen@outlook.com>
2021-03-05 14:17:23 +0200

    11.9 done

--- cypress/integration/pokemon_app.js
+++ cypress/integration/pokemon_app.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Ivysaur page can be opened', function() {
+    cy.contains('ivysaur').click()
+    //cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
